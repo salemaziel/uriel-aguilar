c83ef99ff7f5cb546a7af6186b829f0e7d7003ae
salemaziel <mymainemail0501@gmail.com>
2020-11-22 14:37:28 -0800

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: 'Uriel Aguilar',
     author: 'Salem Aziel',
     description: 'Profile for Uriel Aguilar',
+    banner: 'src/images/bg.jpg'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -12,8 +13,8 @@ module.exports = {
         name: 'Uriel Aguilar Profile',
         short_name: 'Aguilar',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#0e71c8',
+        theme_color: '#0e71c8',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
--- src/components/layout.js
+++ src/components/layout.js
@@ -41,8 +41,8 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'description', content: 'Digital Marketing' },
+              { name: 'keywords', content: 'Digital Marketing, sales' },
             ]}
           >
             <html lang="en" />
